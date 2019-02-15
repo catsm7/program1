4b05f8dbd8d595c844c4664059b7ad12c46ebe4c
catsm <catsm7@yahoo.com>
2019-02-15 13:20:26 -0700

    altered the files to combine program1 & program2

--- WebWorker.java
+++ WebWorker.java
@@ -1,3 +1,4 @@
+      
 /**
 * @author Catalina Sanchez-Maes
 * Web worker: an object of this class executes in its own new thread
@@ -28,15 +29,27 @@ import java.util.Date;
 import java.text.DateFormat;
 import java.util.TimeZone;
 import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
 
 public class WebWorker implements Runnable
 {
    private Socket socket;
+   private String fileName;
+
+      public String getFileName(){
+         return fileName;
+      }
+      
+      public void setFileName(String _name){
+         fileName = _name;
+      }
 
 /**
 * Constructor: must have a valid open socket
 * @param s of object Socket
 **/
+
    public WebWorker(Socket s)
    {
       socket = s;
@@ -48,17 +61,29 @@ public class WebWorker implements Runnable
 * assumes that whoever created the worker created it with a valid
 * open socket object.
 **/
+
    public void run()
    {
       System.out.println("Connection incoming.");
       try {
          InputStream  is = socket.getInputStream();
          OutputStream os = socket.getOutputStream();
-      
-         String location = readHTTPRequest(is).trim();
-         if(fileExists(location)){
+        String location = readHTTPRequest(is).trim();
+         if(fileExists(location) && getFileName().contains(".html")){
             writeHTTPHeader(os, "text/html");
             writeContent(os, location);
+         }else if(fileExists(location) && getFileName().contains(".gif")){
+            writeHTTPHeader(os, "image/gif");
+            writeImageContent(os, location);
+         }else if(fileExists(location) && getFileName().contains(".jpeg")){
+            writeHTTPHeader(os, "image/jpeg");
+            writeImageContent(os, location);
+         }else if(fileExists(location) && getFileName().contains(".png")){
+            writeHTTPHeader(os, "image/png");
+            writeImageContent(os, location);
+         }else if(fileExists(location) && getFileName().contains(".ico")){
+            writeHTTPHeader(os, "image/x-icon");
+            writeImageContent(os, location);
          }else if (location.equals("") || location.equals(null)){
             writeHTTPHeader(os, "text/html");
             os.write("<html><head></head><body>\n".getBytes());
@@ -67,7 +92,7 @@ public class WebWorker implements Runnable
          }else{
             write404Error(os, "text/html", location);
          }
-
+         
          os.flush();
          socket.close();
       
@@ -81,28 +106,52 @@ public class WebWorker implements Runnable
 /**
 * Checks existance of file within user's directory
 * @param location the string of the file path
+* recognizes the file types .html, .png, .jpeg, .gif, .ico
 **/
 
-   private boolean fileExists(String location)
+ private boolean fileExists(String location)
    {
-      String fName;
-      if(location.length() > 4){
+      String fName = "";
+      if(location.length() > 3){
+         System.out.println(fName);
          if ((location.substring(location.length()-5, location.length()).equals(".html"))){
             fName = (System.getProperty("user.dir")+(location)).trim();
+            setFileName(fName);
+            System.out.println(fName);
+         }else if ((location.substring(location.length()-4, location.length()).equals(".gif"))){
+            fName = (System.getProperty("user.dir")+(location)).trim();
+            setFileName(fName);
+            System.out.println(fName);
+         }else if ((location.substring(location.length()-4, location.length()).equals(".png"))){
+            fName = (System.getProperty("user.dir")+(location)).trim();
+            setFileName(fName);
+            System.out.println(fName);
+         }else if ((location.substring(location.length()-5, location.length()).equals(".jpeg"))){
+            fName = (System.getProperty("user.dir")+(location)).trim();
+            setFileName(fName);
+            System.out.println(fName);
+         }else if ((location.substring(location.length()-4, location.length()).equals(".ico"))){
+            fName = (System.getProperty("user.dir")+(location)).trim();
+            setFileName(fName);
+            System.out.println(fName);
          }else{
             fName = (System.getProperty("user.dir")+(location)).trim()+".html";
-         }  
+            setFileName(fName);
+            System.out.println(fName);
+         }
       }else{
          fName = (System.getProperty("user.dir")+(location)).trim()+".html";
+         setFileName(fName);
+         System.out.println(fName);
       }
       File file = new File (fName);
       return file.exists() && !file.isDirectory() && file.isFile();
    }
-      
+   
 /**
 * Read the HTTP request header.
 **/
-
+   
    private String readHTTPRequest(InputStream is)
    {
       String line, result = "";
@@ -113,7 +162,7 @@ public class WebWorker implements Runnable
                Thread.sleep(1);
             }
             line = r.readLine();
-            if(line.contains("GET") && !line.contains("GET / HTTP/1.1")){
+            if(line.contains("GET") && !line.contains("GET / HTTP/1.1") ){
                result = line.substring(4, line.length()-8);
             }
             System.out.println("Request line: ("+line+")");
@@ -192,13 +241,15 @@ public class WebWorker implements Runnable
 * Write file content to the client 
 * @precondition HTTPHeader must be called first 
 * @param os the OutputStream object
-* @param the string of the file location
+* @param the string of the file location\
+* recognizes the file type .html
 **/
+
    private void writeContent(OutputStream os, String location) throws Exception
    {
       String fName;
       if(location.length() > 4){
-         if ((location.substring(location.length()-5, location.length()).equals(".html"))){
+         if ((location.substring(location.length()-5, location.length()).contains(".html"))){
             fName = (System.getProperty("user.dir")+(location)).trim();
          }else{
             fName = (System.getProperty("user.dir")+(location)).trim()+".html";
@@ -214,5 +265,39 @@ public class WebWorker implements Runnable
       while(input.hasNextLine()){
          os.write((input.nextLine().replace("<cs371date>", dform.format(date)).replace("<cs371server>", "User's workstation")).getBytes());
       }
-   }
-} // end class
+   }   
+      
+/**
+* Write file image content to the client 
+* @precondition HTTPHeader must be called first 
+* @param os the OutputStream object
+* @param the string of the file location
+* recognizes image file types .ico, .png, .jpeg, .gif
+* takes the input stream into file into a byte array and output stream to print byte array
+**/
+
+   private void writeImageContent(OutputStream os, String location) throws Exception
+   {
+      String fName = "";
+      if(location.length() > 3){
+         if ((location.substring(location.length()-4, location.length()).equals(".png"))){
+            fName = (System.getProperty("user.dir")+(location)).trim();
+         }else if (location.substring(location.length()-5, location.length()).equals(".jpeg")){
+            fName = (System.getProperty("user.dir")+(location)).trim();
+         }else if (location.substring(location.length()-4, location.length()).equals(".gif")){
+            fName = (System.getProperty("user.dir")+(location)).trim();
+         }else if (location.substring(location.length()-4, location.length()).equals(".ico")){
+            fName = (System.getProperty("user.dir")+(location)).trim();
+         }else{
+            fName = (System.getProperty("user.dir")+(location)).trim()+".ico";
+         } 
+      }
+      
+      File file = new File(getFileName());
+      FileInputStream fileRead = new FileInputStream(file);
+      int fileLength = (int) file.length();
+      byte[] result = new byte[fileLength];
+      fileRead.read(result);
+      os.write(result);
+    } 
+}
